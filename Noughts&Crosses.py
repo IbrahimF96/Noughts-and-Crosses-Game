339fde336f4bb398c4c8eda0fc42ff573c944787
IbrahimF96 <54409852+IbrahimF96@users.noreply.github.com>
2020-11-23 20:24:16 +0000

    Update Noughts&Crosses.py

--- Noughts&Crosses.py
+++ Noughts&Crosses.py
@@ -1,64 +1,77 @@
 import random
 import sys
-import numpy as np
 
-# Today we are going to make an interactive game in Python.
-# This is going to be a game of noughts and crosses!
+print("Welcome to Noughts and Crosses! Let's Play!")
+SIDE_LENGTH = 3
 
-#  Initialise grid and collection of X's or O's needed to win a game.
-grid = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-print(np.asarray(grid))  # To make the output represent a grid instead of a list of lists.
-row_os = ["O", "O", "O"]
-row_xs = ["X", "X", "X"]
-coords_picked = []
+
+def format_grid(grid):
+    return "\n".join("  ".join(row) for row in grid)
+
+
+grid = [["_"] * SIDE_LENGTH for _ in range(SIDE_LENGTH)]
+row_os = ["O"] * SIDE_LENGTH
+row_xs = ["X"] * SIDE_LENGTH
+coords_picked = set()
+
+print(format_grid(grid))  # To make the output represent a grid instead of a list of lists.
 
 # Create a loop to find all co-ordinates in our grid
-all_coords = []
-for x in range(3):
-    for y in range(3):
-        all_coords.append((x, y))
+all_coords = set()
+for x in range(SIDE_LENGTH):
+    for y in range(SIDE_LENGTH):
+        all_coords.add((x, y))
 
 turn = 0
-remaining_coords = []
+remaining_coords = set()
 
 while turn < 10:
     # Find status of each column
-    column_1 = [row[0] for row in grid]
-    column_2 = [row[1] for row in grid]
-    column_3 = [row[2] for row in grid]
+    columns = [[row[i]
+               for row in grid]
+               for i in range(SIDE_LENGTH)]
 
     # Find status of 2 diagonals
     main_diag = [row[i] for i, row in enumerate(grid)]
     counter_diag = [row[-i - 1] for i, row in enumerate(grid)]
 
     # Winning conditions for each player
-    if (grid[0] == row_os or grid[1] == row_os or grid[2] == row_os) or (
-            column_1 == row_os or column_2 == row_os or column_3 == row_os) or (main_diag == row_os) or (
-            counter_diag == row_os):
+    if any((any(row == row_os for row in grid),
+            any(col == row_os for col in columns),
+            any(diag == row_os for diag in [main_diag, counter_diag]))):
         print("The computer has won!")
         sys.exit()
-    elif (grid[0] == row_xs or grid[1] == row_xs or grid[2] == row_xs) or (
-            column_1 == row_xs or column_2 == row_xs or column_3 == row_xs) or (main_diag == row_xs) or (
-            counter_diag == row_xs):
+    elif any((any(row == row_xs for row in grid),
+              any(col == row_xs for col in columns),
+              any(diag == row_xs for diag in [main_diag, counter_diag]))):
         print("Well done, you have won!")
         sys.exit()
     else:
         # If nobody has won yet, take another turn
         if turn % 2 == 0:
-            x_coord = input()
-            y_coord = input()
-            coords_picked.append((int(x_coord), int(y_coord)))
-            grid[int(x_coord)][int(y_coord)] = "X"
-            print(np.asarray(grid))
+            # Checking for invalid inputs
+            while True:
+                try:
+                    x_coord = int(input("\nPlease enter your x coordinate: \n"))
+                    y_coord = int(input("Please enter your y coordinate: \n"))
+                    if (x_coord, y_coord) in coords_picked:
+                        raise ValueError
+                    grid[x_coord][y_coord] = "X"
+                    break
+                except (IndexError, ValueError):
+                    print("Oops! That was not valid input, please input a number in the grid")
+
+            coords_picked.add((x_coord, y_coord))
+            print(format_grid(grid))
             turn += 1
         else:
             # Computer randomly picks a co-ordinate in the grid that has not been picked yet
-            remaining_coords = list(set(all_coords) - set(coords_picked))
-            comp_coords = random.choice(remaining_coords)
+            remaining_coords = all_coords - coords_picked
+            comp_coords = random.choice(list(remaining_coords))
             grid[comp_coords[0]][comp_coords[1]] = "O"
-            coords_picked.append((comp_coords[0], comp_coords[1]))
-            print(comp_coords)
-            print(np.asarray(grid))
+            coords_picked.add((comp_coords[0], comp_coords[1]))
+            print("\n")
+            print(format_grid(grid))
             turn += 1
 
 print("A draw?! We must play again!")
